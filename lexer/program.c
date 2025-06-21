2e71e6f00a329813060854db065e83ebc78cfe23
68mschmitt <68mschmitt@gmail.com>
2025-06-21 15:55:03 -0400

    Basic token type enum and token struct

--- lexer/program.c
+++ lexer/program.c
@@ -1,5 +1,20 @@
 #include <stdio.h>
 
+typedef enum {
+    TOKEN_INT,
+    TOKEN_RETURN,
+    TOKEN_IDENTIFIER,
+    TOKEN_NUMBER,
+    TOKEN_SYMBOL,
+    TOKEN_EOF,
+} TokenType;
+
+typedef struct {
+    TokenType type;
+    char text[64];
+    int value;
+} Token;
+
 int main() {
     printf("Hello, World!\n");
     return 0;
